f9ac13b24e5a992c70461be4fff9935691425414
Irfan Chowdhury <irfanchowdhury80@gmail.com>
2023-09-01 03:37:21 +0600

    Readme Update

---
+++ resources/views/layout/main_partials/footer.blade.php
@@ -0,0 +1,9 @@
+
+@php $general_settings = DB::table('general_settings')->latest()->first(); @endphp
+
+<footer class="main-footer">
+    <div class="container-fluid">
+        <p>&copy; {{$general_settings->site_title ?? "no title"}} || {{ __('Developed by')}}
+            <a href={{$general_settings->footer_link}} class="external">{{$general_settings->footer}}</a> || Version - {{env('VERSION')}}
+    </div>
+</footer>
