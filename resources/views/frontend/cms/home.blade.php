89024a040cf72b900203a74edf1cba9467b3bed1
Irfan Chowdhury <irfanchowdhury80@gmail.com>
2023-10-31 15:59:21 +0600

    Tenant - migration Part hide

--- resources/views/frontend/cms/home.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('frontend.Layout.master')
-
-@section('title_front','Home')
-
-@section('content')
-<div class="container">
-    {!!  html_entity_decode($home) !!}
-</div>
-@endsection
--- resources/views/shared/flash_message.blade.php
+++
@@ -1,6 +0,0 @@
-@if(session()->has('msg'))
-    <div class="alert alert-{{session('type')}} alert-dismissible text-center">
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span>
-        </button>{!! session('msg') !!}
-    </div>
-@endif
